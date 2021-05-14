8a0dba1eac5e8353dcc0eae1e94a06f040e00f41
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-05-15 01:10:05 +0200

    Initial server running

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+
+// Define port
+app.set('port', Number(process.env.PORT) || 5000);
+
+app.listen(app.get('port'), () => {
+    console.log(`Server running in ${app.get('port')}`);
+});
