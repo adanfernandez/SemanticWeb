d7abf4010e9f8d74edcdc1c8d21bdd327e91730d
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2021-05-16 23:07:48 +0200

    Titles of each coach

--- persistence/repository.js
+++ persistence/repository.js
@@ -37,11 +37,18 @@ module.exports = {
     },
 
     /**
-     * 
+     * Get coaches obtained from wikidata with their titles 
      * @param {*} functionCallback 
      */
     getTitlesByCoach: function(functionCallback) {
-
-    }
+        var query = 'Select tcoach.name, ttitle.name, thistorictitle.date from tcoach,ttitle, thistorictitle where thistorictitle.id_title = ttitle.id and thistorictitle.date <= tcoach.final_date and thistorictitle.date >= tcoach.initial_date ORDER BY thistorictitle.date';
+        this.connection.query(query, (err, result) => {
+            if (err) {
+                functionCallback(null);
+            } else {
+                functionCallback(result);
+            }
+        });
+    },
 
 };
